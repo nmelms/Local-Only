309a65ec8dbe8dddc7fe4097bfd2bc420e061150
Nick Melms <nick@craftedagency.com>
2024-01-05 14:27:41 -0500

    position location btn

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -4,22 +4,22 @@ import "leaflet/dist/leaflet.css";
 import coffeeShops from "../data/data.json";
 
 const OpenStreetMap = () => {
-  const ZOOM_LEVEL = 9; // You can change this depending on how zoomed in you want the map to be initially
-  const [center, setCenter] = useState({ lat: 39.5, lng: -98.35 }); // The geographical center of the contiguous United States
+  const ZOOM_LEVEL = 9;
+  const [mapPosition, setMapPostion] = useState({ lat: 39.5, lng: -98.35 });
 
   const markerIcon = new L.Icon({
-    iconUrl: "/map-marker.png", // Assuming you have marker-icon.png in the public folder
+    iconUrl: "/map-marker.png",
     iconSize: [25, 25],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
   });
-  console.log(coffeeShops);
+
   return (
     <MapContainer
-      center={center}
+      center={mapPosition}
       zoom={ZOOM_LEVEL}
-      style={{ height: "90vh", width: "100%" }}
+      className="map-container"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 "use client";
 import Image from "next/image";
-import styles from "./page.module.css";
 import OpenStreetMap from "./components/Map";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { faCrosshairs } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // ... other imports
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <OpenStreetMap />
+      <FontAwesomeIcon
+        className="position-btn ps-2"
+        size="2x"
+        icon={faCrosshairs}
+      />
     </main>
   );
 }
