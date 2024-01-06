5121e296bf68e21d3654ae383ab26c7c370ef796
Nick Melms <nick@craftedagency.com>
2024-01-06 13:52:17 -0500

    display mapbox map and show clusters of dummy data

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,63 +1,148 @@
-import React, { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import coffeeShops from "../data/data.json";
-import useUserStore from "../useUserStore.jsx";
+import React, { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 
-const OpenStreetMap = () => {
-  const ZOOM_LEVEL = 9;
-  const { lat, lng } = useUserStore();
-  const mapRef = useRef(null);
-
+const MapboxMap = () => {
   useEffect(() => {
-    if (mapRef.current && lat != null && lng != null) {
-      mapRef.current.flyTo([lat, lng], 15);
-    }
-  }, [lat, lng]);
+    console.log("mapp render");
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
-  const markerIcon = new L.Icon({
-    iconUrl: "/map-marker.png",
-    iconSize: [25, 25],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.5, 40],
+      zoom: 9,
+    });
 
-  const userIcon = new L.Icon({
-    iconUrl: "/map-marker.png",
-    iconSize: [25, 25],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
+    map.on("load", () => {
+      // Add a new source from our GeoJSON data and
+      // set the 'cluster' option to true. GL-JS will
+      // add the point_count property to your source data.
+      map.addSource("coffeeshops", {
+        type: "geojson",
+        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+        data: "/data.geojson",
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+      });
 
-  return (
-    <MapContainer
-      center={{ lat: 39.5, lng: -98.35 }}
-      zoom={ZOOM_LEVEL}
-      ref={mapRef}
-      className="map-container"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-      />
-      <Marker position={{ lat, lng }} icon={userIcon}></Marker>
-      {coffeeShops.map((shop, index) => (
-        <Marker
-          key={index}
-          position={{ lat: shop.lat, lng: shop.lng }}
-          icon={markerIcon}
-        >
-          <Popup>
-            <strong>{shop.title}</strong>
-            <p>{shop.description}</p>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "coffeeshops",
+        filter: ["has", "point_count"],
+        paint: {
+          // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+          // with three steps to implement three types of circles:
+          //   * Blue, 20px circles when point count is less than 100
+          //   * Yellow, 30px circles when point count is between 100 and 750
+          //   * Pink, 40px circles when point count is greater than or equal to 750
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+        },
+      });
+
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "coffeeshops",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      });
+
+      map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "coffeeshops",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff",
+        },
+      });
+    });
+
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on("click", "unclustered-point", (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const mag = e.features[0].properties.mag;
+      const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
+
+      // Ensure that if the map is zoomed out such that
+      // multiple copies of the feature are visible, the
+      // popup appears over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
+        .addTo(map);
+    });
+
+    map.on("click", "clusters", (e) => {
+      const features = map.queryRenderedFeatures(e.point, {
+        layers: ["clusters"],
+      });
+      const clusterId = features[0].properties.cluster_id;
+      map
+        .getSource("coffeeshops")
+        .getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom,
+          });
+        });
+    });
+
+    map.on("mouseenter", "clusters", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+    map.on("mouseleave", "clusters", () => {
+      map.getCanvas().style.cursor = "";
+    });
+    // Add geolocate control to the map.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+  }, []);
+
+  return <div id="map" style={{ height: "90vh" }} />;
 };
 
-export default OpenStreetMap;
+export default MapboxMap;
--- app/page.js
+++ app/page.js
@@ -1,40 +1,42 @@
 "use client";
 import Image from "next/image";
-import OpenStreetMap from "./components/Map";
+import MapboxMap from "./components/Map";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 import { faCrosshairs } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useUserStore from "./useUserStore";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { lat, lng, setLocation, spinning, setSpinning } = useUserStore();
+  // const { lat, lng, setLocation, spinning, setSpinning } = useUserStore();
 
-  const handlePositionClick = () => {
-    setSpinning(true);
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation(position.coords.latitude, position.coords.longitude);
-      });
-    } else {
-      document.getElementById("location").innerHTML =
-        "Geolocation is not supported by this browser.";
-    }
-  };
+  // const handlePositionClick = () => {
+  //   setSpinning(true);
+  //   if ("geolocation" in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       setLocation(position.coords.latitude, position.coords.longitude);
+  //     });
+  //   } else {
+  //     document.getElementById("location").innerHTML =
+  //       "Geolocation is not supported by this browser.";
+  //   }
+  // };
 
-  useEffect(() => {
-    setSpinning(false);
-  }, [lat, lng]);
-
+  // useEffect(() => {
+  //   setSpinning(false);
+  // }, [lat, lng]);
+  console.log("rnder");
   return (
     <main>
-      <OpenStreetMap />
-      <FontAwesomeIcon
+      <MapboxMap />
+      {/* <FontAwesomeIcon
         onClick={() => handlePositionClick()}
         className={`position-btn ${spinning ? "spin" : ""}`}
         size="2x"
         icon={faCrosshairs}
-      />
+      /> */}
     </main>
   );
 }
