9ce96f6086b493a5e71d1ef34b2186830e3c6cdf
Nick Melms <nick@craftedagency.com>
2024-01-06 18:49:22 -0500

    hide offcanvas onclick

--- app/components/ShopOffcanvas.jsx
+++ app/components/ShopOffcanvas.jsx
@@ -4,15 +4,15 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import useUserStore from "../useUserStore";
 
 function ShopOffcanvas() {
-  const { showOffcanvas } = useUserStore();
+  const { showOffcanvas, setShowOffcanvas } = useUserStore();
   const [show, setShow] = useState(showOffcanvas);
-  const handleClose = () => setShow(false);
+  const handleClose = () => setShowOffcanvas(false);
 
   return (
     <>
       <Offcanvas show={showOffcanvas} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Add A Shop</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           Some text as placeholder. In real life you can have the elements you
