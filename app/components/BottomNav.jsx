cbe8ebe05120edc5c6c47c87bda407ed46d9cb82
Nick Melms <nick@craftedagency.com>
2024-01-06 14:48:04 -0500

    add plus icon w/ click handler

--- app/components/BottomNav.jsx
+++ app/components/BottomNav.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const BottomNav = () => {
+  const handleAddClick = () => {
+    console.log("click");
+  };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-4">col 6</div>
+    <div className="container bottom-nav">
+      <div className="row h-100 ">
+        <div className="col-4 d-flex justify-content-center align-items-center">
+          <FontAwesomeIcon
+            onClick={() => handleAddClick()}
+            className="plus-icon"
+            icon={faPlus}
+          />
+        </div>
         <div className="col-8">col 6</div>
       </div>
     </div>
