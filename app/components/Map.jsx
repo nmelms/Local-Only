35a988ce004612152bf4a64b5405ada8add0cb75
Nick Melms <nick@craftedagency.com>
2024-01-06 14:07:39 -0500

    up size of unclusted marker

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -14,17 +14,12 @@ const MapboxMap = () => {
     });
 
     map.on("load", () => {
-      // Add a new source from our GeoJSON data and
-      // set the 'cluster' option to true. GL-JS will
-      // add the point_count property to your source data.
       map.addSource("coffeeshops", {
         type: "geojson",
-        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
         data: "/data.geojson",
         cluster: true,
-        clusterMaxZoom: 14, // Max zoom to cluster points on
-        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+        clusterMaxZoom: 14,
+        clusterRadius: 50,
       });
 
       map.addLayer({
@@ -33,11 +28,6 @@ const MapboxMap = () => {
         source: "coffeeshops",
         filter: ["has", "point_count"],
         paint: {
-          // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
-          // with three steps to implement three types of circles:
-          //   * Blue, 20px circles when point count is less than 100
-          //   * Yellow, 30px circles when point count is between 100 and 750
-          //   * Pink, 40px circles when point count is greater than or equal to 750
           "circle-color": [
             "step",
             ["get", "point_count"],
@@ -78,25 +68,18 @@ const MapboxMap = () => {
         filter: ["!", ["has", "point_count"]],
         paint: {
           "circle-color": "#11b4da",
-          "circle-radius": 4,
+          "circle-radius": 8,
           "circle-stroke-width": 1,
           "circle-stroke-color": "#fff",
         },
       });
     });
 
-    // When a click event occurs on a feature in
-    // the unclustered-point layer, open a popup at
-    // the location of the feature, with
-    // description HTML from its properties.
     map.on("click", "unclustered-point", (e) => {
       const coordinates = e.features[0].geometry.coordinates.slice();
       const mag = e.features[0].properties.mag;
       const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
 
-      // Ensure that if the map is zoomed out such that
-      // multiple copies of the feature are visible, the
-      // popup appears over the copy being pointed to.
       while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
         coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
       }
