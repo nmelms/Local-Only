2166dab1d4f955c58621eb898e006cdd811994a9
Nick Melms <nick@craftedagency.com>
2024-01-06 19:00:28 -0500

    add form to offcanvas

--- app/components/ShopOffcanvas.jsx
+++ app/components/ShopOffcanvas.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import useUserStore from "../useUserStore";
+import Form from "react-bootstrap/Form";
 
 function ShopOffcanvas() {
   const { showOffcanvas, setShowOffcanvas } = useUserStore();
@@ -15,8 +16,38 @@ function ShopOffcanvas() {
           <Offcanvas.Title>Add A Shop</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicStreet">
+              <Form.Control type="text" placeholder="Street address" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCity">
+              <Form.Control type="text" placeholder="City" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicState">
+              <Form.Control type="text" placeholder="State" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicZip">
+              <Form.Control type="text" placeholder="Zip code" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicDetails">
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter details"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Offcanvas.Body>
       </Offcanvas>
     </>
