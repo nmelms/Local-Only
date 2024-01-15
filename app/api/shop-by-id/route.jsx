9d9f21957f56f74bb2b36db224217b70f8e4edfa
Nick Melms <nick@craftedagency.com>
2024-01-15 15:10:54 -0500

    display and update picutre on click

---
+++ app/api/shop-by-id/route.jsx
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { supabase } from "../../lib/supabaseClient";
+
+export async function GET(request) {
+  const searchParams = request.nextUrl.searchParams;
+  const shopId = searchParams.get("id");
+
+  async function fetchShopById(id) {
+    const { data, error } = await supabase
+      .from("locations")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.log("there was an error");
+      return null;
+    }
+    return data;
+  }
+
+  let shopData = await fetchShopById(shopId);
+  return NextResponse.json(shopData);
+}
--- app/components/StorePopup.jsx
+++ app/components/StorePopup.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import Link from "next/link";
+import toTitleCase from "../lib/toTitleCase";
 
 const StorePopup = ({ popupData }) => {
-  return <div className="store-popup ">{popupData.properties.name}</div>;
+  const [shopData, setShopData] = useState();
+
+  useEffect(() => {
+    fetch(`/api/shop-by-id?id=${popupData.properties.id}`, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => setShopData(data));
+  }, [popupData]);
+
+  console.log(shopData, "shop data");
+
+  return (
+    <div className="store-popup container">
+      <div className="row">
+        <span className="popup-title">
+          {toTitleCase(popupData.properties.name)}
+        </span>
+      </div>
+      <div className="row h-100">
+        <div className="col-6 img-wrapper">
+          <img
+            src={`https://xlvjgjhetfrtaigrimtd.supabase.co/storage/v1/object/public/${shopData?.imageURL}`}
+            className="popup-image"
+          />
+        </div>
+
+        <div className="col-6 d-flex justify-content-center align-items-center">
+          <Link href={`/store/${shopData?.id}`}>Info</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StorePopup;
