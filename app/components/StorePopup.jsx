ed17de3cefeaad1fcede79a52e831967c22be0f6
Nick Melms <nick@craftedagency.com>
2024-01-15 15:37:32 -0500

    style image on popup

--- app/components/StorePopup.jsx
+++ app/components/StorePopup.jsx
@@ -16,14 +16,14 @@ const StorePopup = ({ popupData }) => {
   console.log(shopData, "shop data");
 
   return (
-    <div className="store-popup container">
+    <div className="store-popup container d-flex flex-column">
       <div className="row">
         <span className="popup-title">
           {toTitleCase(popupData.properties.name)}
         </span>
       </div>
-      <div className="row h-100">
-        <div className="col-6 img-wrapper">
+      <div className="row flex-grow-1">
+        <div className="col-6 img-wrapper h-100 ">
           <img
             src={`https://xlvjgjhetfrtaigrimtd.supabase.co/storage/v1/object/public/${shopData?.imageURL}`}
             className="popup-image"
