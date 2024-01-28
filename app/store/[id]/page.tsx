f068ec1c21f325436473f454a04ee94a09593c9c
Nick Melms <nick@craftedagency.com>
2024-01-27 22:28:30 -0500

    add descriptive text

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -44,9 +44,17 @@ const page = async ({ params }: PageProps) => {
         Navigate
       </a>
       <img
-        src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+999(${shopData.lng},${shopData.lat})/${shopData.lng},${shopData.lat},14/400x400?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
+        className="static-store-map"
+        src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+999(${shopData.lng},${shopData.lat})/${shopData.lng},${shopData.lat},15/400x400?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
         alt="Map"
       />
+      <p className="description-text">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt nihil
+        laudantium et facere! Dolorem excepturi nam magnam ducimus recusandae
+        facere corrupti cumque culpa quas natus fugit quibusdam unde eius
+        commodi sit exercitationem laudantium quia temporibus, odio aperiam at,
+        non ullam.
+      </p>
     </div>
   );
 };
